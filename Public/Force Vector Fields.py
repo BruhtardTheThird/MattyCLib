2346c9eb01c011f8d1b53b9bdfd84926d2c30feb
Matty C. <71330200+BruhtardTheThird@users.noreply.github.com>
2024-11-15 15:42:24 -0500

    Pull request-001 (#2)
    * Update and rename Force Vector Fields-Once More.py to Public/Force Vector Fields.py
    
    Moved Folder
    
    * Update and rename MTHFunctions.py to FunctionLib/MTHFunctions.py
    
    Move file.
    
    * Update and rename ParametricGraphing.py to FunctionLib/ParametricGraphing.py
    
    * Update and rename Triple Integrals.py to Public/Triple Integrals.py
    
    * Commit to fix unimportable library.
    
    * Commit to add single-parameter countour integrals
    to FunctionLib.MTHFunctions. Also added code for
    import handling.

--- Public/Force Vector Fields.py
+++ Public/Force Vector Fields.py
@@ -1,6 +1,14 @@
 import sympy as sp
-from MTHFunctions import Jacobian, Div, Curl, findPotntialFnction
-import ParametricGraphing as pmg
+import os.path
+import sys
+
+# go up one directory level from this file's directory:
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# prepend parent directory to the system path:
+sys.path.insert(0, path)
+
+from FunctionLib.MTHFunctions import Jacobian, Div, Curl, findPotntialFnction
+import FunctionLib.ParametricGraphing as pmg
 import matplotlib.pyplot as plt
 import numpy as np
 x, y, z, u, v, w = sp.symbols('x,y,z,u,v,w')
@@ -11,7 +19,7 @@ sin, cos, tan, sqrt, e= sp.sin, sp.cos, sp.tan, sp.sqrt, sp.exp
 
 Field1 = sp.Array([
     3*x**3,
-    5*y**5,
+    5*x**5+y**3,
     -2*x*y+y
     ])
 
@@ -35,9 +43,11 @@ curl1 = Curl(Field1,VarList=varList1)
 ecurl1 = Curl(Field1,Point1,varList1)
 print('Curl expression is:\n',curl1,'\nCurl evaluation is:\n',ecurl1)
 try:
-    potFun = findPotntialFnction(Field1,VarList=varList1)
-    print('A potential function is: \n',potFun)
-except:
-    print('Field:\n',Field1,'\nIs not conservative. Therefore a potential function does not exist for the field.')
+    raise Exception
+    #potFun = findPotntialFnction(Field1,VarList=varList1)
+    #print('A potential function is: \n',potFun)
+except Exception:
+    print('\nyatta.')
+    #print('Field:\n',Field1,'\nIs not conservative. Therefore a potential function does not exist for the field.')
 pmg.NumpyParaGraph(Field1,varTupleList1,parBoundsTupleList1)
 plt.show()
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MattyCLib
-Library for math stuff I am working on in python. 
+Library for math stuff I am working on in python.
+FunctionLib contains, of course, the main library of functions, which currently range from evaluating curl and divergence to plotting a 3-dimensional graph of a parametric function.
