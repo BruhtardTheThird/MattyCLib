ca09b9673d90179e809b93097f0ff7513d1314dd
Matty C. <matrimcn@gmail.com>
2024-11-22 17:19:31 -0500

    Refactored a few functions to be more concise and readable. Removed a few reduntant for loops.

--- FunctionLib/MTHFunctions.py
+++ FunctionLib/MTHFunctions.py
@@ -6,54 +6,41 @@ x, y, z, u, v, w = sp.symbols('x,y,z,u,v,w')
 def InteriorProduct(Array1, Array2):
     if len(Array1) != len(Array2):
         raise ValueError('The arrays do not have the same length!')
-    productExpr = 0
-    for i in range(len(Array1)):
-        productExpr = productExpr + Array1[i]*Array2[i]
+    productExpr = sum([Array1[i]*Array2[i] for i in range(len(Array1))])
     return productExpr
 def CrossProduct(Array1, Array2):
     if len(Array1) != len(Array2):
-        raise ValueError('The arrays do not have the same length!')
-    if len(Array1) == 3:
-        ProductList = [
-            sp.trigsimp(Array1[2]*Array2[1]-Array1[1]*Array2[2]),
-            sp.trigsimp(Array1[0]*Array2[2]-Array1[2]*Array2[0]),
-            sp.trigsimp(Array1[1]*Array2[0]-Array1[0]*Array2[1])
-            ]
-        return ProductList
-    if len(Array1)==2:
-        ProductList = [
-            0,
-            0,
-            sp.trigsimp(Array1[0]*Array2[1]-Array1[1]*Array2[0])
+        raise ValueError('Arrays must have the same length.')
+
+    if len(Array1) == 3:  # 3D
+        return [
+            sp.trigsimp(Array1[2]*Array2[1] - Array1[1]*Array2[2]),
+            sp.trigsimp(Array1[0]*Array2[2] - Array1[2]*Array2[0]),
+            sp.trigsimp(Array1[1]*Array2[0] - Array1[0]*Array2[1])
         ]
-        return ProductList
+    elif len(Array1) == 2:  # 2D
+        return [0, 0, sp.trigsimp(Array1[0]*Array2[1] - Array1[1]*Array2[0])]
     else:
-        raise IndexError('Expected arrays of length 2 or 3, instead got arrays of length:',len(Array1))
+        raise ValueError("Only 2D or 3D vectors are supported.")
 def Magnitude(ArrayLike):
-    MagExp = 0
-    for i in ArrayLike:
-        MagExp = MagExp + i**2
-    ReturnExp = sp.sqrt(sp.simplify(MagExp))
+    ReturnExp = sp.sqrt(sp.simplify(sum([i**2 for i in ArrayLike])))
     return ReturnExp
 def Jacobian(MappedField,VarList):
-    """For a transformation T(x,y,z) = T(u,v,w), this function defines the Jacobian,
-    taking x, y, and sometimes z as functions of u, v, and sometimes w."""
-    preMatrix = []
+    """Returns the determinant of the Jacobian matrix for a given parametrization and variables."""
     if len(VarList) != len(MappedField):
-        raise ValueError('The determinant of a non-square matrix is not defined in euclidian geometry.') 
-    for i in MappedField:
-        preMatrix.append([diff(i,j) for j in VarList])
-    matrix = sp.Matrix(preMatrix)
-    return sp.trigsimp(matrix.det())
+        raise ValueError('MappedField and VarList must have the same length.')
+    
+    # Compute the Jacobian matrix
+    jacobian_matrix = sp.Matrix([[diff(f, v) for v in VarList] for f in MappedField])
+    return sp.trigsimp(jacobian_matrix.det())
+
 def Div(Field,Point=None,VarList=None):
     """For a force vector field, F = <P,Q,R> and a point O = <x,y,z> (R&z = 0 for 2D) the divergence of F at point O is
     equal to the dot product of F and Del. Both arguments are input as lists of functions in terms of x, y, and z."""
-    divExpr = sp.S.Zero
     if VarList is None:
         VarList = [x,y,z]
         warnings.warn('WARNING: Absence of VarList argument might break the output, as variables are set to [x,y,z]!',UserWarning)
-    for f, s in zip(Field,VarList):
-        divExpr = divExpr + diff(f,s)
+    divExpr = sum([diff(f,s) for f,s in zip(Field,VarList)])
     if Point == None:
         return divExpr
     else:
@@ -61,60 +48,44 @@ def Div(Field,Point=None,VarList=None):
             divExpr = divExpr.subs([(s,p)])
         return divExpr
 def Curl(Field,Point=None,VarList=None):
-    """For a force vector field, F = <P,Q,R> and a point O = <x,y,z> (R&z = 0 for 2D), the curl of F at point O is
-    equal to the cross product of F and Del. Both arguments are input as lists of functions in terms of x, y, and z."""
-    if VarList is None:
-        VarList = [x,y,z]
-        warnings.warn('WARNING: Absence of VarList argument might break the output, as variables are set to [x,y,z]!',UserWarning)
+    """Compute the curl of a vector field. Field is a sympy array or list, Point and VarList are lists."""
+    VarList = VarList or [x, y, z]
+    gList = [[diff(f, v) for v in VarList] for f in Field]
+    
+    if len(gList) == 3:
+        curl_array = [
+            sp.trigsimp(gList[2][1] - gList[1][2]),
+            sp.trigsimp(gList[0][2] - gList[2][0]),
+            sp.trigsimp(gList[1][0] - gList[0][1])
+        ]
     else:
-        gList = []
-        for i in Field:
-            iDiff = []
-            for j in VarList:
-                iDiff.append(sp.trigsimp(diff(i,j)))
-            gList.append(iDiff)
-            curlAray = sp.Array([gList])
-    if len(curlAray) == 3:
-        curlAray = sp.Array([sp.trigsimp(gList[2][1]-gList[1][2]),
-                    sp.trigsimp(gList[0][2]-gList[2],[0]),
-                    sp.trigsimp(gList[1][0]-gList[0][1])])
-    else:
-        curlAray = sp.Array([0,0,sp.trigsimp(gList[1][0]-gList[0][1])])
+        curl_array = [0, 0, sp.trigsimp(gList[1][0] - gList[0][1])]
+
     if Point is None:
-        return curlAray
+        return curl_array
     else:
-        curlEval = []
-        for i,j,k in zip(VarList,Point,curlAray):
-            curlEval.append(k.subs(i,j))
+        curlEval = [(k.subs(i,j)) for i,j,k in zip(VarList,Point,curl_array)]
         return curlEval
-def findPotntialFnction(Field,VarList=None):
+def FindPotentialFnction(Field,VarList=None):
     if Curl(Field,VarList=VarList).tolist()!=[0]*len(Field):
         raise ValueError('Field:',Field,'Is not conservative. Therefore such a function does not exist.')
     if VarList is None:
         VarList = [x,y,z]
         warnings.warn('WARNING: Absence of VarList argument might break the output, as variables are set to [x,y,z]!',UserWarning)
+    NewList = []
     for f,s in zip(Field,VarList):
         for i in sp.expand(sp.integrate(f,s)).as_ordered_terms():
-            if i not in VarList:
-                VarList.append(i)
-    potenExpr = sp.S.Zero
-    for i in VarList:
-        potenExpr = potenExpr + i
+            if i not in NewList:
+                NewList.append(i)
+    potenExpr = sum([i for i in NewList])
     return potenExpr
 def ParametrizeExpr(Expression,MappedArray,VarList=None):
-    """Returns an expression where the values of the mapping array have been substituted for the variables in the expression. If VarList is left blank,
-    the function will substitute the values of the mapping array with the variables it 'sees' first. VarList should be input as a list of variables
-    that are used in the original function."""
-    if VarList != None:
+    """Substitute variables in Expression using MappedArray or VarList."""
+    if VarList is not None:
         if len(MappedArray) != len(VarList):
-            raise ValueError('The parametrization array:',MappedArray,'does not have the same dimension as the input:',VarList)
-        for i,j in zip(VarList,MappedArray):
-            Expression = Expression.subs(i,j)
-        return Expression
-    else:
-        for i,j in zip(Expression.free_symbols,MappedArray):
-            Expression = Expression.subs(i,j)
-        return Expression
+            raise ValueError('MappedArray and VarList must have the same length.')
+        return Expression.subs(zip(VarList, MappedArray))
+    return Expression.subs(zip(Expression.free_symbols, MappedArray))
 def CountourInt(Field,VarList,VarBounds=None,Eval=False,Hard=True,ParaField=None,ParVar=None,ParVars=None,ParVarBounds=None,):
     if Hard == True and ParVars == None:
         ParametrizedField = []
@@ -133,12 +104,12 @@ def CountourInt(Field,VarList,VarBounds=None,Eval=False,Hard=True,ParaField=None
         if VarBounds==None:
             StokesIntegrand = (InteriorProduct(Curl(Field,VarList=VarList),[1,1,1])).subs(zip(VarList,ParaField))
             if ParaField == [sp.Symbol('r')*sp.cos(sp.Symbol('theta')),sp.Symbol('r')*sp.sin(sp.Symbol('theta'))] or ParaField == [sp.Symbol('r')*sp.cos(sp.Symbol('theta')),sp.Symbol('r')*sp.sin(sp.Symbol('theta')),sp.Symbol('z')]:
-                StokesIntegrand = StokesIntegrand*sp.Symbol('r')
+                StokesIntegrand *= sp.Symbol('r')
             StokesIntegral = sp.Integral(sp.simplify(StokesIntegrand),(ParVars[0],ParVarBounds[0]),(ParVars[1],ParVarBounds[1]))
-            if Eval == False:
-                print('Integrand for ContourInt =\n',StokesIntegrand)
-                return StokesIntegral
-            return StokesIntegral.doit()
+            if Eval:
+                return StokesIntegral.doit()
+            print('Integrand for ContourInt =\n',StokesIntegrand)
+            return StokesIntegral
         StokesIntegrand = InteriorProduct(Curl(Field,VarList=VarList),[1,1,1])
         StokesIntegral = sp.Integral(StokesIntegrand,(VarList[0],VarBounds[0]),(VarList[1],VarBounds[1]))
         if Eval == False:
@@ -172,17 +143,13 @@ def SurfArea(Surface,VarList,ParaField=list|None,ParVarList=list|None,Eval=True,
     else:
         return Integrand
 def SurfFlux(Surface,Field,VarList,ParaVarList,Bounds=list|None,Eval=True,Left=True,Upward=False):
-    ParaField = []
-    for i in Field:
-        ParaField.append(i.subs(zip(VarList,Surface)))
-    NormalDiffs = []
-    for j in range(2):
-        NormalDiffs.append([sp.trigsimp(diff(Surface[i],ParaVarList[j])) for i in range(3)])
+    ParaField = [f.subs(zip(VarList, Surface)) for f in Field]
+    NormalDiffs = [[sp.trigsimp(diff(Surface[i], ParaVarList[j])) for i in range(3)] for j in range(2)] # gets the partial derivatives of the normal vector.
     Integrand = sp.factor(InteriorProduct(CrossProduct(NormalDiffs[0],NormalDiffs[1]),ParaField))
-    if Upward == True:
-        Integrand = Integrand*-1
-    if Eval == True:
-        if Left == True:
+    if Upward:
+        Integrand *= -1
+    if Eval:
+        if Left:
             Integral = sp.Integral(Integrand,(ParaVarList[0],Bounds[0]),(ParaVarList[1],Bounds[1]))
         else:
             Integral = sp.Integral(Integrand,(ParaVarList[1],Bounds[1]),(ParaVarList[0],Bounds[0]))
